27f9dd22ffcdb559d07ced233a5f5f9e63bb1e17
Yury Pliner <yury.pliner@gmail.com>
2016-10-15 14:34:14 +0500

    Сделал стабы, в которые нужно будет вписать реализации

---
+++ Source/Lockness/HarrisLinkedList.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace CodeDonkeys.Lockness
+{
+    public sealed class HarrisLinkedList<TElement> : ISet<TElement>
+    {
+        public IEnumerator<TElement> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public bool Add(TElement element)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Contains(TElement element)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Remove(TElement element)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Source/Lockness/ISet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CodeDonkeys.Lockness
+{
+    public interface ISet<TElement> : IEnumerable<TElement>
+    {
+        bool Add(TElement element);
+
+        bool Contains(TElement element);
+
+        bool Remove(TElement element);
+    }
+}
