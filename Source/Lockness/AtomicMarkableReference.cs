9f59f9f1739923c71bb82fb70a2cf4781071d8ba
Yury Pliner <yury.pliner@gmail.com>
2016-10-15 14:21:45 +0500

    Добавил два ссылочных примитива

---
+++ Source/Lockness/AtomicMarkableReference.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading;
+
+namespace CodeDonkeys.Lockness
+{
+    internal sealed class AtomicMarkableReference<TReference>
+    {
+        public AtomicMarkableReference(TReference reference, bool mark)
+        {
+            state = new State(reference, mark);
+        }
+
+        public bool CompareAndSet(TReference expectedReference, TReference newReference, bool expectedMark, bool newMark)
+        {
+            var localState = state;
+
+            return ReferenceEquals(localState.Reference, expectedReference)
+                   && localState.Mark == expectedMark
+                   && Interlocked.CompareExchange(ref state, new State(newReference, newMark), localState) == localState;
+        }
+
+        public void Set(TReference newReference, bool newMark)
+        {
+            Interlocked.Exchange(ref state, new State(newReference, newMark));
+        }
+
+        public TReference Get(out bool mark)
+        {
+            var localState = state;
+            mark = localState.Mark;
+            return localState.Reference;
+        }
+
+        private volatile State state;
+
+        private class State
+        {
+            internal readonly TReference Reference;
+            internal readonly bool Mark;
+
+            public State(TReference reference, bool mark)
+            {
+                Reference = reference;
+                Mark = mark;
+            }
+        }
+    }
+}
