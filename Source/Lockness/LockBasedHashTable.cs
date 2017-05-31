6f4d48d5f09b31803a76ef18e906d788d057b1cb
SvalovaNastya <Sun-7402@mail.ru>
2017-05-31 20:44:42 +0500

    сделала в конструкторе неправильный comparer

--- Source/Lockness/LockBasedHashTable.cs
+++ Source/Lockness/LockBasedHashTable.cs
@@ -4,12 +4,30 @@ using System.Collections.Generic;
 
 namespace CodeDonkeys.Lockness
 {
+    internal class HashTableComparer<TElement> : IEqualityComparer<TElement>
+    {
+        private readonly IComparer<TElement> internalComperer;
+        public HashTableComparer(IComparer<TElement> elementComparer)
+        {
+            internalComperer = elementComparer;
+        }
+
+        public bool Equals(TElement x, TElement y)
+        {
+            return internalComperer.Compare(x, y) == 0;
+        }
+
+        public int GetHashCode(TElement obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
     public sealed class LockBasedHashTable<TElement> : ISet<TElement>
     {
         private readonly HashSet<TElement> list;
-        public LockBasedHashTable(IEqualityComparer<TElement> elementComparer)
+        public LockBasedHashTable(IComparer<TElement> elementComparer)
         {
-            list = new HashSet<TElement>(elementComparer);
+            list = new HashSet<TElement>(new HashTableComparer<TElement>(elementComparer));
         }
 
         public IEnumerator<TElement> GetEnumerator()
